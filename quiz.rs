e4911684996284c5bad0440da275171c05c29352
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2017-12-02 14:03:52 +0900

    save quiz program in case.

---
+++ quiz.rs
@@ -0,0 +1,43 @@
+// Q1. What is the entry point/function of Rust?
+
+
+
+// Q2. Which function is correct to return added value?
+
+// fn add_1(a, b) -> i32 {
+//   a + b
+// }
+
+// fn add_2(a: i32, b: i32) -> () {
+//   a + b
+// }
+
+// fn add_3(a: i32, b: i32) -> i32 {
+//   a + b
+// }
+
+// fn add_4(a: i32, b: i32) -> () {
+//   a + b;
+// }
+
+// fn add_5(a: i32, b: i32) -> i32 {
+//   a + b;
+// }
+
+// fn main() {
+//   // println!("{}", add_1(1,2) );
+//   // println!("{}", add_2(1,2) );
+//   // println!("{}", add_3(1,2) );
+//   // println!("{}", add_4(1,2) );
+//   // println!("{}", add_5(1,2) );
+// }
+
+
+// Q3 How do you assign value into a variable and change value of the variable?
+fn main() {
+  // Create a variable named whatever you want.
+
+  // Change value of the variable
+
+  // Print the value with "println!" macro
+}
